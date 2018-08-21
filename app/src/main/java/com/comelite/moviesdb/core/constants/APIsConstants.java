dec53db6e466a338f2b7caae9d10f36cc78f321b
Mohamed Elkhateeb <mohamed.magdy-elshaarawy@vodafone.com>
2018-08-21 14:00:26 +0200

    Added first commit for Github

---
+++ app/src/main/java/com/comelite/moviesdb/core/constants/APIsConstants.java
@@ -0,0 +1,10 @@
+package com.comelite.moviesdb.core.constants;
+
+public class APIsConstants {
+    public final static String BASE_URL = "https://fake-poi-api.mytaxi.com";
+
+    public final static String P1_LAT = "p1Lat";
+    public final static String P1_LON = "p1Lon";
+    public final static String P2_LAT = "p2Lat";
+    public final static String P2_LON = "p2Lon";
+}
---
+++ app/src/main/java/com/comelite/moviesdb/core/di/modules/AppModule.java
@@ -0,0 +1,23 @@
+package com.comelite.moviesdb.core.di.modules;
+
+import android.app.Application;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class AppModule {
+    Application mApplication;
+
+    public AppModule(Application mApplication) {
+        this.mApplication = mApplication;
+    }
+
+    @Provides
+    @Singleton
+    Application provideApplication() {
+        return mApplication;
+    }
+}
